4fcfea324a3cf621e009b374bab9c908667f7509
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-26 21:31:40 +0530

    [AkankshaAnil] trying to print elements using Generic

--- GenericP13/Program.cs
+++ GenericP13/Program.cs
@@ -1,42 +1,21 @@
 ﻿internal class Program
 {
-    public static void toPrint(int[] inputArray)
-    {
-        foreach (int element in inputArray)
-        {
-            Console.WriteLine(element);
-        }
-        Console.WriteLine("--------------");
-    }
-
-    public static void toPrint(double[] inputArray)
-    {
-        foreach (double element in inputArray)
-        {
-            Console.WriteLine(element);
-        }
-        Console.WriteLine("--------------");
-    }
-    public static void toPrint(char[] inputArray)
-    {
-        foreach (char element in inputArray)
-        {
-            Console.WriteLine(element);
-        }
-        Console.WriteLine("--------------");
-    }
-
-
-
-
     static void Main(String[] args)
     {
         int[] intArray = { 1, 2, 3, 4, 5, };
         double[] doubleArray = { 1.1, 2.2, 3.3, 4.5, 5.5, };
-        char[] charArray = { 'a', 'b', 'c', 'd', 'e', };
+        char[] charArray = { 'H','E', 'L', 'L', 'O', };
 
-        Program.toPrint(intArray);
-        Program.toPrint(doubleArray);
-        Program.toPrint(charArray);
+        Program.toPrint<int>(intArray);
+        Program.toPrint<double>(doubleArray);
+        Program.toPrint<char>(charArray);
     }
+    public static void toPrint<T>(T[] inputArray)
+    {
+        foreach (var element in inputArray)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("--------------");
+    }    
 }
