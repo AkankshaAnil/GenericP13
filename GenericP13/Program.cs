3ba3d959ec29d4c302be2b9d55a2412832b8af86
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-26 21:17:31 +0530

    [AkankshaAnil] using toPrint method to print elements

--- GenericP13/Program.cs
+++ GenericP13/Program.cs
@@ -1,7 +1,42 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    public static void toPrint(int[] inputArray)
     {
-        Console.WriteLine("Hello, Akanksha");
+        foreach (int element in inputArray)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("--------------");
+    }
+
+    public static void toPrint(double[] inputArray)
+    {
+        foreach (double element in inputArray)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("--------------");
+    }
+    public static void toPrint(char[] inputArray)
+    {
+        foreach (char element in inputArray)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("--------------");
+    }
+
+
+
+
+    static void Main(String[] args)
+    {
+        int[] intArray = { 1, 2, 3, 4, 5, };
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.5, 5.5, };
+        char[] charArray = { 'a', 'b', 'c', 'd', 'e', };
+
+        Program.toPrint(intArray);
+        Program.toPrint(doubleArray);
+        Program.toPrint(charArray);
     }
 }
