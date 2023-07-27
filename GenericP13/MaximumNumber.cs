c77340c5b48b5be7e977093e41769298074ef3b2
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-27 18:42:01 +0530

    [AkankshaAnil] adding PrintMax Generic Method

---
+++ GenericP13/MaximumNumber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericP13
+{
+    internal class MaximumNumber
+    {
+        public static int MaximumIntNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+        public static string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+            throw new Exception("FirstNumber,SecondNumber,ThirdNumber are same");
+        }
+
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+            {
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+                {
+                    return thirdValue;
+                }
+                return firstValue;
+
+                throw new Exception("FirstNumber,SecondNumber,ThirdNumber are same");
+            }
+
+        }
+    }
+
--- GenericP13/Program.cs
+++ GenericP13/Program.cs
@@ -7,7 +7,24 @@ internal class Program
     {
         Console.WriteLine("Find Maximum Number");
 
-        string output2 = MaxStringNumber.MaximumStringNumber("Apple", "peach", "Banana");
-        Console.WriteLine("Max String Value is: " + output2);
+        int output = MaximumNumber.MaximumIntNumber(10, 30, 20);
+        Console.WriteLine(output);
+ 
+        double output1 = MaximumNumber.MaximumFloatNumber(1.1, 3.0, 2.0);
+        Console.WriteLine(output1);
+
+        String output2 = MaximumNumber.MaximumStringNumber("Apple", "peach", "banana");
+        Console.WriteLine(output2);
+
+        Console.WriteLine("*****************");
+
+        int[] intArray = { 123, 256, 376, 444, 543, };
+        GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+        generic.PrintMaxValue();
+
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.5, 5.5, };
+        GenericMaximum<double> generic1 = new GenericMaximum<double>(doubleArray);
+        generic1.PrintMaxValue();
+
     }
 }
